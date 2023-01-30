e223a894b992f7f4fe31beb50e63dab4c9b61961
Kristina Peshne <peshne.k@docsvision.com>
2023-01-30 11:41:30 +0300

    TSK-3546 Доработать кнопки Подписать и Скачать доверенность

--- PowersOfAttorneyWebExtension/src/EventHandlers/ExportPowerOfAttorney.ts
+++ PowersOfAttorneyWebExtension/src/EventHandlers/ExportPowerOfAttorney.ts
@@ -3,12 +3,24 @@ import { $CardId } from "@docsvision/webclient/System/LayoutServices";
 import { $PowerOfAttorneyApiController } from '@docsvision/webclient/Generated/DocsVision.WebClient.Controllers';;
 import { $PowersOfAttorneyDemoController } from "../ServerRequests.ts/PowersOfAttorneyDemoController";
 
-export const exportPowerOfAttorney = async (sender: CustomButton) => {
+export const exportPowerOfAttorneyWithSignature = async (sender: CustomButton) => {
     const powerOfAttorneyUserCardId = sender.layout.getService($CardId);
     const powerOfAttorneyId = await sender.layout.getService($PowersOfAttorneyDemoController).getPowerOfAttorneyCardId(powerOfAttorneyUserCardId);
     try {
         const url  = sender.layout.params.services.urlResolver.resolveApiUrl("exportPowerOfAttorney", "powerOfAttorneyApi");
         const request = `${url}?powerOfAttorneyId=${powerOfAttorneyId}&withSignature=true`;
+        sender.layout.params.services.fileDownload.download(request);
+    } catch (err) {
+        throw err;
+    } 
+}
+
+export const exportPowerOfAttorneyWithoutSignature = async (sender: CustomButton) => {
+    const powerOfAttorneyUserCardId = sender.layout.getService($CardId);
+    const powerOfAttorneyId = await sender.layout.getService($PowersOfAttorneyDemoController).getPowerOfAttorneyCardId(powerOfAttorneyUserCardId);
+    try {
+        const url  = sender.layout.params.services.urlResolver.resolveApiUrl("exportPowerOfAttorney", "powerOfAttorneyApi");
+        const request = `${url}?powerOfAttorneyId=${powerOfAttorneyId}&withSignature=false`;
         sender.layout.params.services.fileDownload.download(request);
     } catch (err) {
         throw err;
