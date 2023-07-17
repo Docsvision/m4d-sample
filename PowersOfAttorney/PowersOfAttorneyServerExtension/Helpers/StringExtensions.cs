e59c26f2fa8a16d9442a852e069d88b602ed1f33
andreyt <andreyt@109PC0101>
2023-07-17 14:10:03 +0300

    ERR-5218 : try fix null reference exception

--- PowersOfAttorney/PowersOfAttorneyServerExtension/Helpers/StringExtensions.cs
+++ PowersOfAttorney/PowersOfAttorneyServerExtension/Helpers/StringExtensions.cs
@@ -4,7 +4,7 @@
     {
         public static string AsNullable(this string content)
         {
-            return string.IsNullOrEmpty(content.Trim()) ? null : content;
+            return string.IsNullOrWhiteSpace(content) ? null : content;
         }
     }
 }
