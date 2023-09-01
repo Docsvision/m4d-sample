8a56cd98341320532a8c470de3e7df5d0b69af7c
Vladimir Sorokin <sorokin.v@docsvision.com>
2023-09-01 14:22:29 +0300

    ERR-5548 Ошибка при подписи из задания на подписание PowerOfAttroneyCardId

--- PowersOfAttorney/PowersOfAttorneyWebExtension/src/EventHandlers/SignAndSendPowerOfAttorneyToRegistrationAsFileFromTask.ts
+++ PowersOfAttorney/PowersOfAttorneyWebExtension/src/EventHandlers/SignAndSendPowerOfAttorneyToRegistrationAsFileFromTask.ts
@@ -1,7 +1,10 @@
 import { OperationExecutingEventArgs } from "@docsvision/webclient/BackOffice/OperationExecutingEventArgs";
 import { $LayoutCardController } from "@docsvision/webclient/Generated/DocsVision.WebClient.Controllers";
+import { CardLink } from "@docsvision/webclient/Platform/CardLink";
 import { LayoutControl } from "@docsvision/webclient/System/BaseControl";
 import { ICancelableEventArgs } from "@docsvision/webclient/System/ICancelableEventArgs";
+import { layoutManager } from "@docsvision/webclient/System/LayoutManager";
+import { $ControlStore } from "@docsvision/webclient/System/LayoutServices";
 import { sendPowerOfAttorneyToRegistrationAsFile } from "./PowerOfAttorneyRegistration";
 import { signPowerOfAttorneyFromTask, SIGN_OPERATION_ID } from "./SignPowerOfAttorneyFromTask";
 
@@ -9,6 +12,8 @@ import { signPowerOfAttorneyFromTask, SIGN_OPERATION_ID } from "./SignPowerOfAtt
 export const signAndSendPowerOfAttorneyToRegistrationAsFileFromTask = async (sender: LayoutControl, e: ICancelableEventArgs<OperationExecutingEventArgs>) => {
     if (e.data.operationData.builtInOperationId === SIGN_OPERATION_ID) {
         await signPowerOfAttorneyFromTask(sender, e, false);
+        const powerOfAttorneyIdControl = sender.layout.getService($ControlStore).get<CardLink>("powerOfAttorneySysCard");
+        await powerOfAttorneyIdControl.reloadFromServer();
         await sendPowerOfAttorneyToRegistrationAsFile(sender);
         const signOperationIdPOA = sender.layout.controls.locationContainer.params.layoutModel.layoutModel.layoutInfo.operations.find(operation => operation.alias === "Sign").id;
         const powerOfAttorneyUserCardId = sender.layout.controls.locationContainer.params.layoutModel.cardInfo.id;
