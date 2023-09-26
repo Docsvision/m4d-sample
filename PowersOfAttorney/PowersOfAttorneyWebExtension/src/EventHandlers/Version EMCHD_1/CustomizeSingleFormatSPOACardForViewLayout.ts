88f488d29a0597ffb172c15d95a32d8cdc88f7d0
Mkrtchyan Artem <mkrtchyan.ar@docsvision.com>
2023-09-26 16:19:15 +0300

    TSK-5092 МЧД. Сэмпл. Доработать скрипт для powersType1

--- PowersOfAttorney/PowersOfAttorneyWebExtension/src/EventHandlers/Version EMCHD_1/CustomizeSingleFormatSPOACardForViewLayout.ts
+++ PowersOfAttorney/PowersOfAttorneyWebExtension/src/EventHandlers/Version EMCHD_1/CustomizeSingleFormatSPOACardForViewLayout.ts
@@ -1,3 +1,5 @@
+import { Dropdown } from "@docsvision/webclient/Platform/Dropdown";
+import { TextArea } from "@docsvision/webclient/Platform/TextArea";
 import { LayoutControl } from "@docsvision/webclient/System/BaseControl";
 
 export const customizeSingleFormatSPOACardForViewLayout = (sender: LayoutControl) => {
@@ -7,6 +9,8 @@ export const customizeSingleFormatSPOACardForViewLayout = (sender: LayoutControl
     const textPowersDescr = controls.textPowersDescr;
     const substPOABasis = controls.substPOABasis;
     const parentalPOABlock = controls.parentalPOABlock;
+    const powersType1: Dropdown = controls.powersType1;
+    const textPowersDescr1: TextArea = controls.textPowersDescr1;
 
     if (powersType.params.value === "humReadPower") {
         refPowersTable.params.visibility = false;
@@ -14,6 +18,14 @@ export const customizeSingleFormatSPOACardForViewLayout = (sender: LayoutControl
     } else {
         refPowersTable.params.visibility = true;
         textPowersDescr.params.visibility = false;
+    }
+
+    if (powersType1.params.value === "humReadPower") {
+        refPowersTable.params.visibility = false;
+        textPowersDescr1.params.visibility = true;
+    } else {
+        refPowersTable.params.visibility = true;
+        textPowersDescr1.params.visibility = false;
     }
 
     if (substPOABasis.params.value) {
