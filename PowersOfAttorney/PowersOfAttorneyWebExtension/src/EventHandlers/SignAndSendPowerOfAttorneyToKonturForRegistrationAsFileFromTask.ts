0f4138ec2930b8bb35eef5ff0f4ca82bc3c2481b
Olga Nozdrya <Nozdrya.O@docsvision.com>
2023-12-21 15:05:27 +0300

    ERR-6344 Не запрашивается подпись в задании на подписание мчд при повторной отправке на подписание

--- PowersOfAttorney/PowersOfAttorneyWebExtension/src/EventHandlers/SignAndSendPowerOfAttorneyToKonturForRegistrationAsFileFromTask.ts
+++ PowersOfAttorney/PowersOfAttorneyWebExtension/src/EventHandlers/SignAndSendPowerOfAttorneyToKonturForRegistrationAsFileFromTask.ts
@@ -4,19 +4,21 @@ import { CardLink } from "@docsvision/webclient/Platform/CardLink";
 import { ICancelableEventArgs } from "@docsvision/webclient/System/ICancelableEventArgs";
 import { $ControlStore } from "@docsvision/webclient/System/LayoutServices";
 import { signPowerOfAttorneyFromTask } from "./SignPowerOfAttorneyFromTask";
-import { SING_OPERATION_COMPLETION_OPTION_ID } from "../PowerOfAttorneyConstants";
+import { SIGN_OPERATION_ID, SING_OPERATION_COMPLETION_OPTION_ID } from "../PowerOfAttorneyConstants";
 import { CustomButton } from "@docsvision/webclient/Platform/CustomButton";
 import { sendForRegistrationToRegistry } from "./SendForRegistrationToRegistry";
+import { GenModels } from "@docsvision/webclient/Generated/DocsVision.WebClient.Models";
 
 export const signAndSendPowerOfAttorneyToKonturForRegistrationAsFileFromTask = async (sender: CustomButton, e: ICancelableEventArgs<OperationExecutingEventArgs>) => {
-    if (e.data.operationData.completionOptionId === SING_OPERATION_COMPLETION_OPTION_ID) {
-        await signPowerOfAttorneyFromTask(sender, e, false);
-        const powerOfAttorneyIdControl = sender.layout.getService($ControlStore).get<CardLink>("powerOfAttorneySysCard");
-        await powerOfAttorneyIdControl.reloadFromServer();
-        await sendForRegistrationToRegistry(sender);
-        const signOperationIdPOA = sender.layout.controls.locationContainer.params.layoutModel.layoutModel.layoutInfo.operations.find(operation => operation.alias === "Sign").id;
-        const powerOfAttorneyUserCardId = sender.layout.controls.locationContainer.params.layoutModel.cardInfo.id;
-        await sender.layout.getService($LayoutCardController).changeState({ cardId: powerOfAttorneyUserCardId, operationId: signOperationIdPOA, timestamp: sender.layout.controls.locationContainer.params.layoutModel.cardInfo.timestamp, comment: "", layoutParams: sender.layout.controls.locationContainer.params.layoutModel.layoutModel.layoutInfo.layoutParams });
-        e.accept();
+    if (e.data.operationData.completionOptionId === SIGN_OPERATION_ID 
+        && e.data.operationData.additionalInfo.decisionSemantics === GenModels.DecisionSemantics.Positive) {
+            await signPowerOfAttorneyFromTask(sender, e, false);
+            const powerOfAttorneyIdControl = sender.layout.getService($ControlStore).get<CardLink>("powerOfAttorneySysCard");
+            await powerOfAttorneyIdControl.reloadFromServer();
+            await sendForRegistrationToRegistry(sender);
+            const signOperationIdPOA = sender.layout.controls.locationContainer.params.layoutModel.layoutModel.layoutInfo.operations.find(operation => operation.alias === "Sign").id;
+            const powerOfAttorneyUserCardId = sender.layout.controls.locationContainer.params.layoutModel.cardInfo.id;
+            await sender.layout.getService($LayoutCardController).changeState({ cardId: powerOfAttorneyUserCardId, operationId: signOperationIdPOA, timestamp: sender.layout.controls.locationContainer.params.layoutModel.cardInfo.timestamp, comment: "", layoutParams: sender.layout.controls.locationContainer.params.layoutModel.layoutModel.layoutInfo.layoutParams });
+            e.accept();
     }
 }
