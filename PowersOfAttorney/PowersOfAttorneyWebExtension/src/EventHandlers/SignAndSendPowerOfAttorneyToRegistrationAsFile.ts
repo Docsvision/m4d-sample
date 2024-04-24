dd0c45bcd06c7533f66ccef829bbfd1628c27830
Bogatyrev.P <Bogatyrev.P@docsvision.com>
2024-04-24 13:26:03 +0300

    ERR-7088 После отмены отправки доверенность остается в состоянии Сформирована

--- PowersOfAttorney/PowersOfAttorneyWebExtension/src/EventHandlers/SignAndSendPowerOfAttorneyToRegistrationAsFile.ts
+++ PowersOfAttorney/PowersOfAttorneyWebExtension/src/EventHandlers/SignAndSendPowerOfAttorneyToRegistrationAsFile.ts
@@ -6,7 +6,7 @@ import { signPowerOfAttorney } from "./SignPowerOfAttorney";
 
 export const signAndSendPowerOfAttorneyToRegistrationAsFile = async (sender: CustomButton) => {
     await signPowerOfAttorney(sender, false);
-    await sendPowerOfAttorneyToRegistrationAsFile(sender);
     const operationId = sender.layout.layoutInfo.operations.find(operation => operation.alias === "Sign").id;
     await sender.layout.changeState(operationId);
+    await sendPowerOfAttorneyToRegistrationAsFile(sender);
 }
