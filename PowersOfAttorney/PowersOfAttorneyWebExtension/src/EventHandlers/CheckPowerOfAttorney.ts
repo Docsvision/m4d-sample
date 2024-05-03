6188212830aeeea78ec5fad4bf6f197248d01118
Olga Nozdrya <Nozdrya.O@docsvision.com>
2024-05-03 17:32:42 +0300

    ERR-7038 Отображается пустое окно с ошибкой после отправки доверенности

--- PowersOfAttorney/PowersOfAttorneyWebExtension/src/EventHandlers/CheckPowerOfAttorney.ts
+++ PowersOfAttorney/PowersOfAttorneyWebExtension/src/EventHandlers/CheckPowerOfAttorney.ts
@@ -2,6 +2,7 @@ import { CustomButton } from "@docsvision/webclient/Platform/CustomButton";
 import { $ApplicationSettings } from "@docsvision/webclient/StandardServices";
 import { $PowersOfAttorneyButtonController } from "../ServerRequests/PowersOfAttorneyButtonController";
 import { MessageBox } from "@docsvision/webclient/Helpers/MessageBox/MessageBox";
+import { $MessageWindow } from "@docsvision/web/components/modals/message-box";
 
 export const checkPowerOfAttorney = async (sender: CustomButton) => {
     const powerOfAttorneyId = sender.layout.controls.powerOfAttorneySysCard.params.value?.cardId;
@@ -10,11 +11,10 @@ export const checkPowerOfAttorney = async (sender: CustomButton) => {
 
     const employeeId = sender.layout.getService($ApplicationSettings).employee.id;
 
-    powersOfAttorneyButtonController?.checkPowerOfAttorney(powerOfAttorneyId, employeeId).then(msg => {
-        if (msg.Success) {
-            msg.Data && MessageBox.ShowInfo(msg.Data);
-        } else {
-            MessageBox.ShowError(msg.Message);
-        }
-    });
+    try {
+        const msg = await powersOfAttorneyButtonController?.checkPowerOfAttorney(powerOfAttorneyId, employeeId)
+        sender.layout.getService($MessageWindow).showInfo(msg);
+    } catch(err) {
+        sender.layout.getService($MessageWindow).showError(err?.message || err);
+    }
 };
--- PowersOfAttorney/PowersOfAttorneyWebExtension/src/EventHandlers/SendForRegistrationToRegistry.ts
+++ PowersOfAttorney/PowersOfAttorneyWebExtension/src/EventHandlers/SendForRegistrationToRegistry.ts
@@ -4,19 +4,19 @@ import { $PowersOfAttorneyButtonController } from "../ServerRequests/PowersOfAtt
 import { MessageBox } from "@docsvision/webclient/Helpers/MessageBox/MessageBox";
 import { signPowerOfAttorney } from "./SignPowerOfAttorney";
 import { resources } from "@docsvision/webclient/System/Resources";
+import { $MessageWindow } from "@docsvision/web/components/modals/message-box";
 
 export const sendForRegistrationToRegistry = async (sender: CustomButton) => {
     const powerOfAttorneyId = sender.layout.controls.powerOfAttorneySysCard.params.value?.cardId;
     const powersOfAttorneyButtonController = sender.layout.getService($PowersOfAttorneyButtonController);
     const employeeId = sender.layout.getService($ApplicationSettings).employee.id;
 
-    powersOfAttorneyButtonController?.sendForRegistrationToRegistry(powerOfAttorneyId, employeeId).then(msg => {
-        if (msg.Success) {
-            msg.Data && MessageBox.ShowInfo(resources.M4DRegistry_Register_Success);
-        } else {
-            MessageBox.ShowError(msg.Message);
-        }
-    });
+    try {
+        await powersOfAttorneyButtonController?.sendForRegistrationToRegistry(powerOfAttorneyId, employeeId);
+        sender.layout.getService($MessageWindow).showInfo(resources.M4DRegistry_Register_Success);
+    } catch(err) {
+        sender.layout.getService($MessageWindow).showError(err?.message || err);
+    }
 };
 
 export const signAndSendForRegistrationToRegistry = async (sender: CustomButton) => {
@@ -26,11 +26,11 @@ export const signAndSendForRegistrationToRegistry = async (sender: CustomButton)
     const powersOfAttorneyButtonController = sender.layout.getService($PowersOfAttorneyButtonController);
     const employeeId = sender.layout.getService($ApplicationSettings).employee.id;
 
-    const msg = await powersOfAttorneyButtonController?.sendForRegistrationToRegistry(powerOfAttorneyId, employeeId);
-    if (msg.Success) {
-        MessageBox.ShowInfo(resources.M4DRegistry_Register_Success);
-    } else {
-        MessageBox.ShowError(msg.Message);
+    try {
+        await powersOfAttorneyButtonController?.sendForRegistrationToRegistry(powerOfAttorneyId, employeeId);
+        sender.layout.getService($MessageWindow).showInfo(resources.M4DRegistry_Register_Success);
+    } catch(err) {
+        sender.layout.getService($MessageWindow).showError(err?.message || err);
     }
 
     const operationId = sender.layout.layoutInfo.operations.find(operation => operation.alias === "Sign").id;
