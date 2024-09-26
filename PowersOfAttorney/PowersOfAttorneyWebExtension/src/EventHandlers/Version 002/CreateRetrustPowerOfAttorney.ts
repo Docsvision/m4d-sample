b24cb4d55d4be106f611539a034b8e1a78224884
Bogatyrev.P <Bogatyrev.P@docsvision.com>
2024-09-26 14:37:40 +0300

    ERR-7750 Возникает ошибка при повторном формировании доверенности

--- PowersOfAttorney/PowersOfAttorneyWebExtension/src/EventHandlers/Version 002/CreateRetrustPowerOfAttorney.ts
+++ PowersOfAttorney/PowersOfAttorneyWebExtension/src/EventHandlers/Version 002/CreateRetrustPowerOfAttorney.ts
@@ -12,13 +12,6 @@ export const createRetrustPowerOfAttorney = async (sender: CustomButton) => {
     const powerOfAttorneyUserCardId = sender.layout.getService($CardId);
     const powerOfAttorneyId = sender.layout.controls.powerOfAttorneySysCard.params.value?.cardId;    
     await sender.layout.getService($PowersOfAttorneyDemoController).createRetrustPowerOfAttorney(powerOfAttorneyUserCardId);
-    if (powerOfAttorneyId) {
-        const cardInfo = await sender.layout.getService($PowerOfAttorneyApiController).getPowerOfAttorneyInfo(powerOfAttorneyId);
-        const resources = sender.layout.getService($Resources);
-        if (cardInfo.status === resources.PowerOfAttorney_StatusPreparation) {
-            sender.layout.getService($LayoutCardController).delete({cardId: powerOfAttorneyId, isNew: false})
-        }  
-    }
     const operationId = sender.layout.layoutInfo.operations.find(operation => operation.alias === "Create").id;
     await sender.layout.getService($LayoutCardController).changeState({cardId:powerOfAttorneyUserCardId, operationId: operationId, timestamp: sender.layout.cardInfo.timestamp, comment: "", layoutParams: sender.layout.layoutInfo.layoutParams});
     sender.layout.getService($Router).refresh();
