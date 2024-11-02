810f062876517d51b51c3a055e6d9a934a6329d4
Bogatyrev.P <Bogatyrev.P@docsvision.com>
2024-11-02 13:30:51 +0300

    Fixed build.

--- PowersOfAttorney/PowersOfAttorneyWebExtension/copy-path.js
+++ PowersOfAttorney/PowersOfAttorneyWebExtension/copy-path.js
@@ -2,11 +2,6 @@ const ROOT = process.env.SamplesOutput || "./../SamplesOutput"
 const SITE_ROOT = `${ROOT}/Site`;
 const EXTENSION_NAME = "PowersOfAttorneyWebExtension";
 const MODULES_DIR = SITE_ROOT + "/Content/Modules";
-const PLUGINS = [`src/DocumentSignBatchOperation/DocumentSignBatchOperationDesignerExtension/SignPOABatchOperationControl.xml`];
-const PLUGINS_DIR = `${ROOT}/Plugins`;
-
 
 module.exports.STYLES_DIR =  `${MODULES_DIR}/${EXTENSION_NAME}`;
 module.exports.BUNDLE_DIR =  `${MODULES_DIR}/${EXTENSION_NAME}`;
-module.exports.PLUGINS = PLUGINS;
-module.exports.PLUGINS_DIR = PLUGINS_DIR;
